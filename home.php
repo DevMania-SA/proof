359010109a41ffff03e3fe9fe42f2d2e60313512
Simondink <45486260+Simondink@users.noreply.github.com>
2019-09-04 00:13:44 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,30 @@
+<?php
+
+session_start();
+if(!isset($_SESSION['username'])){
+	header('location:login.php');
+}
+
+?>
+
+<html>
+<head>
+<title> Home Page </title>
+<link rel="stylesheet" type="text/css" href="style.css">
+		 <link rel="stylesheet" type="text/css"
+		 href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+</head>
+<body>
+     
+	 <div class="container">
+	 
+	 
+	<a class="float-right" href="logout.php"> LOGOUT </a> 
+	 
+	 <h1> welcome <?php echo $_SESSION['username']; ?> </h1>
+     
+	 </div>
+	 
+</body>
+</html>
---
+++ registration.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+header('location:login.php');
+
+$con = mysql_connect('localhost','root','123456');
+
+mysqli_select_db($con, 'useregistration');
+
+$name = $_POST['user'];
+$pass =$_POST['password'];
+
+$s = " select * from usertable where name = '$name'";
+
+$result = mysqli_query($con, $s);
+
+$num = mysqli_num_rows($result);
+
+if($num == 1){
+	echo" username already exist";
+
+	
+}else{
+	$reg= " insert into usertable(name , password) values ('$name' , '$password')";
+	mysqli_query($con, $reg);
+	echo" registration successful";
+}
+
+?>
